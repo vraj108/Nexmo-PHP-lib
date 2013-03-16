c233fdcead471e63f617d16484a9aabc5540806d
kathleen <wagner_kathleen61@yahoo.com>
2013-03-17 01:01:20 +0800

    Update example.php

--- example.php
+++ example.php
@@ -1,24 +1,4 @@
-<?php
-
-
-
-	include ( "NexmoMessage.php" );
-
-
-	/**
-	 * To send a text message.
-	 *
-	 */
-
-	// Step 1: Declare new NexmoMessage.
-	$nexmo_sms = new NexmoMessage('f47229b4', 'f1fd89e2');
-
-	// Step 2: Use sendText( $to, $from, $message ) method to send a message. 
-	$info = $nexmo_sms->sendText( '+447937947451', 'MyApp', 'Hello!' );
-
-	// Step 3: Display an overview of the message
-	echo $nexmo_sms->displayOverview($info);
-
-	// Done!
-
-?>
+ $sms = new NexmoMessage('f47229b4', 'f1fd89e2');
+     if ($sms->inboundText()) {
+         $sms->reply('You said: ' . $sms->text);
+     }
