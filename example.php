69b0573aad0ddd1541c1821b4c1cc163d87ccb52
kathleen <wagner_kathleen61@yahoo.com>
2013-05-22 22:07:26 +0800

    Update example.php

--- example.php
+++ example.php
@@ -1,4 +1,4 @@
- $sms = new NexmoMessage('7d7d3db4', '963a7ae0');
+ $sms = new NexmoMessage('7cd9c582', '3ffff4bc');
      if ($sms->inboundText()) {
          $sms->reply('You said: ' . $sms->text);
      }
